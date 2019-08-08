e67622fd2c37e700ada36dbec55452239cd18423
iminyukhin <iminyukhin@iminyukhin-pc.thumbtack.lo>
2019-08-08 12:51:52 +0600

    init commit

---
+++ src/main/java/net/thumbtack/testdevices/TestdevicesApplication.java
@@ -0,0 +1,13 @@
+package net.thumbtack.testdevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdevicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdevicesApplication.class, args);
+    }
+
+}
