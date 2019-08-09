c11ce320e16b2a786650bffd417d19b713e24d25
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-09 16:50:00 +0600

    add user model

---
+++ src/main/java/net/thumbtack/testdevices/core/models/User.java
@@ -0,0 +1,199 @@
+package net.thumbtack.testdevices.core.models;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+    private Set<Authority> authorities;
+
+    public User(
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public User(
+            final Long id,
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public User(
+            final Long id,
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email,
+            final Set<Authority> authorities
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    public User(
+            final Long id,
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email,
+            final String password
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email,
+            final Authority authority
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        Set<Authority> authoritiesList = new HashSet<>();
+        authoritiesList.add(authority);
+        this.authorities = authoritiesList;
+    }
+
+    public User(
+            final String firstName,
+            final String lastName,
+            final String phone,
+            final String email,
+            final String password
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(final String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(final Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getFirstName(), user.getFirstName()) &&
+                Objects.equals(getLastName(), user.getLastName()) &&
+                Objects.equals(getPhone(), user.getPhone()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getAuthorities(), user.getAuthorities());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getPhone(), getEmail(), getPassword(), getAuthorities());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", authorities=" + authorities +
+                '}';
+    }
+}
