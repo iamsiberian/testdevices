09f03d0297292b1c2ad7a186e139d1034918bb3a
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-13 17:14:16 +0600

    #11 add Device service and bean configuration

--- src/main/java/net/thumbtack/testdevices/config/ServiceConfig.java
+++ src/main/java/net/thumbtack/testdevices/config/ServiceConfig.java
@@ -1,7 +1,10 @@
 package net.thumbtack.testdevices.config;
 
 import net.thumbtack.testdevices.core.repositories.AuthoritiesDao;
+import net.thumbtack.testdevices.core.repositories.DeviceDao;
 import net.thumbtack.testdevices.core.repositories.UsersDao;
+import net.thumbtack.testdevices.web.services.DevicesService;
+import net.thumbtack.testdevices.web.services.DevicesServiceImpl;
 import net.thumbtack.testdevices.web.services.UsersService;
 import net.thumbtack.testdevices.web.services.UsersServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +18,12 @@ public class ServiceConfig {
             final UsersDao usersDao
     ) {
         return new UsersServiceImpl(authoritiesDao, usersDao);
+    }
+
+    @Bean
+    public DevicesService devicesService(
+            final DeviceDao deviceDao
+    ) {
+        return new DevicesServiceImpl(deviceDao);
     }
 }
