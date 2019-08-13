b722bcb8176efec404772098bdb5f0ad99983861
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-13 17:14:16 +0600

    #11 add Device service and bean configuration

---
+++ src/main/java/net/thumbtack/testdevices/config/ServiceConfig.java
@@ -0,0 +1,17 @@
+package net.thumbtack.testdevices.config;
+
+import net.thumbtack.testdevices.core.repositories.DeviceDao;
+import net.thumbtack.testdevices.web.services.DevicesService;
+import net.thumbtack.testdevices.web.services.DevicesServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+    @Bean
+    public DevicesService devicesService(
+            final DeviceDao deviceDao
+    ) {
+        return new DevicesServiceImpl(deviceDao);
+    }
+}
---
+++ src/main/java/net/thumbtack/testdevices/web/services/DevicesServiceImpl.java
@@ -0,0 +1,45 @@
+package net.thumbtack.testdevices.web.services;
+
+import net.thumbtack.testdevices.core.models.Device;
+import net.thumbtack.testdevices.core.repositories.DeviceDao;
+import net.thumbtack.testdevices.dto.request.DeviceRequest;
+import net.thumbtack.testdevices.dto.response.DeviceResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DevicesServiceImpl implements DevicesService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DevicesServiceImpl.class);
+    private DeviceDao deviceDao;
+
+    public DevicesServiceImpl(final DeviceDao deviceDao) {
+        this.deviceDao = deviceDao;
+    }
+
+    @Override
+    public DeviceResponse addDevice(final DeviceRequest request) {
+        LOGGER.debug("DevicesServiceImpl addDevice: {}", request);
+        Device device = getDeviceFromDeviceRequest(request);
+        return getDeviceResponseFromDevice(deviceDao.insert(device));
+    }
+
+    DeviceResponse getDeviceResponseFromDevice(final Device device) {
+        return new DeviceResponse(
+                device.getId(),
+                device.getType(),
+                device.getOwner(),
+                device.getModel(),
+                device.getOsType(),
+                device.getDescription()
+        );
+    }
+
+    Device getDeviceFromDeviceRequest(final DeviceRequest request) {
+        return new Device(
+                request.getType(),
+                request.getOwner(),
+                request.getModel(),
+                request.getOsType(),
+                request.getDescription()
+        );
+    }
+}
