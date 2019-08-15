cba1c880e8613839af592db8aec8de2bc4eda083
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-15 18:34:23 +0600

    #12 add Empty response model

---
+++ src/main/java/net/thumbtack/testdevices/dto/response/EmptyResponse.java
@@ -0,0 +1,9 @@
+package net.thumbtack.testdevices.dto.response;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+public class EmptyResponse {
+    public EmptyResponse() {
+    }
+}
