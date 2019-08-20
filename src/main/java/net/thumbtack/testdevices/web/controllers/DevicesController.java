d7e90d458271e78247bba0a0b91bb1aaa6d31392
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-20 21:27:52 +0600

    #19 add give/take device methods

--- src/main/java/net/thumbtack/testdevices/web/controllers/DevicesController.java
+++ src/main/java/net/thumbtack/testdevices/web/controllers/DevicesController.java
@@ -1,16 +1,20 @@
 package net.thumbtack.testdevices.web.controllers;
 
 import net.thumbtack.testdevices.dto.request.DeviceRequest;
+import net.thumbtack.testdevices.web.security.JwtTokenService;
 import net.thumbtack.testdevices.web.services.DevicesService;
+import net.thumbtack.testdevices.web.services.EventsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +25,17 @@ import javax.validation.Valid;
 public class DevicesController {
     private static final Logger LOGGER = LoggerFactory.getLogger(DevicesController.class);
     private DevicesService devicesService;
+    private EventsService eventsService;
+    private JwtTokenService jwtTokenService;
 
-    public DevicesController(final DevicesService devicesService) {
+    public DevicesController(
+            final DevicesService devicesService,
+            final EventsService eventsService,
+            final JwtTokenService jwtTokenService
+    ) {
         this.devicesService = devicesService;
+        this.eventsService = eventsService;
+        this.jwtTokenService = jwtTokenService;
     }
 
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR')")
@@ -37,7 +49,7 @@ public class DevicesController {
     }
 
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR')")
-    @DeleteMapping(path = "/devices/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/devices/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity deleteDevice(final @PathVariable("id") long deviceId) {
         LOGGER.debug("DevicesController deleteDevice {}", deviceId);
         devicesService.deleteDevice(deviceId);
@@ -45,5 +57,27 @@ public class DevicesController {
                 ResponseEntity
                         .ok()
                         .build();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'USER')")
+    @GetMapping(path = "/devices/{id}/take", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity returnDevice(final @RequestHeader("Authorization") String authHeader, final @PathVariable("id") long deviceId) {
+        LOGGER.debug("DevicesController returnDevice {}, from requestHeader: {}", deviceId, authHeader);
+        long userId = jwtTokenService.getUserIdFromAuthHeader(authHeader);
+        return
+                ResponseEntity
+                        .ok()
+                        .body(eventsService.returnDevice(userId, deviceId));
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'USER')")
+    @DeleteMapping(path = "/devices/{id}/give", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity giveDevice(final @RequestHeader("Authorization") String authHeader, final @PathVariable("id") long deviceId) {
+        LOGGER.debug("DevicesController giveDevice {}", deviceId);
+        long userId = jwtTokenService.getUserIdFromAuthHeader(authHeader);
+        return
+                ResponseEntity
+                        .ok()
+                        .body(eventsService.giveDevice(userId, deviceId));
     }
 }
