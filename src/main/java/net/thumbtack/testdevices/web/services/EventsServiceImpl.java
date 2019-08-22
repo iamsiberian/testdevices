26ff01f33699bc4480f08e4e7c87b3007f3e3ca3
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-22 17:22:38 +0600

    #23 refactored take/return device methods

--- src/main/java/net/thumbtack/testdevices/web/services/EventsServiceImpl.java
+++ src/main/java/net/thumbtack/testdevices/web/services/EventsServiceImpl.java
@@ -4,6 +4,8 @@ import net.thumbtack.testdevices.core.models.ActionType;
 import net.thumbtack.testdevices.core.models.Event;
 import net.thumbtack.testdevices.core.repositories.EventsDao;
 import net.thumbtack.testdevices.dto.response.EventResponse;
+import net.thumbtack.testdevices.exceptions.ErrorCode;
+import net.thumbtack.testdevices.exceptions.TestDevicesException;
 import net.thumbtack.testdevices.web.converters.EventDtoToModelConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,14 +28,34 @@ public class EventsServiceImpl implements EventsService {
     @Override
     public EventResponse returnDevice(final long userId, final long deviceId) {
         LOGGER.debug("DevicesServiceImpl returnDevice: {}, by user id : {}", deviceId, userId);
-        Event event = eventsDao.insert(eventDtoToModelConverter.getEventFromDeviceIdUserIdActionType(userId, deviceId, ActionType.RETURN, LocalDateTime.now()));
-        return eventDtoToModelConverter.getEventResponseFromEvent(event);
+        Event event = eventsDao.getLastEventByDeviceIdAndUserId(deviceId, userId);
+        if (event == null) {
+            throw new TestDevicesException(ErrorCode.DEVICE_IS_ALREADY_TAKEN_BUT_NOT_BY_YOU);
+        } else if (event.getActionType().equals(ActionType.RETURN)) {
+            throw new TestDevicesException(ErrorCode.DEVICE_IS_ALREADY_RETURN_BY_YOU);
+        }
+        return
+                eventDtoToModelConverter.getEventResponseFromEvent(
+                        eventsDao.insert(new Event(
+                                userId, deviceId, ActionType.RETURN, LocalDateTime.now()
+                        ))
+                );
     }
 
     @Override
     public EventResponse takeDevice(final long userId, final long deviceId) {
         LOGGER.debug("DevicesServiceImpl takeDevice: {}, by user id : {}", deviceId, userId);
-        Event event = eventsDao.insert(eventDtoToModelConverter.getEventFromDeviceIdUserIdActionType(userId, deviceId, ActionType.TAKE, LocalDateTime.now()));
-        return eventDtoToModelConverter.getEventResponseFromEvent(event);
+        Event event = eventsDao.getLastEventByDeviceIdAndUserId(deviceId, null);
+        if (event != null && event.getActionType().equals(ActionType.TAKE) && event.getUserId().equals(userId)) {
+            throw new TestDevicesException(ErrorCode.DEVICE_IS_ALREADY_TAKEN_BY_YOU);
+        } else if (event != null && event.getActionType().equals(ActionType.TAKE) && !event.getUserId().equals(userId)) {
+            throw new TestDevicesException(ErrorCode.DEVICE_IS_ALREADY_TAKEN_BUT_NOT_BY_YOU);
+        }
+        return
+                eventDtoToModelConverter.getEventResponseFromEvent(
+                        eventsDao.insert(new Event(
+                                userId, deviceId, ActionType.TAKE, LocalDateTime.now()
+                        ))
+                );
     }
 }
