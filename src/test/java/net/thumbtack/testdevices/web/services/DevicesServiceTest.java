256de888ea04628c470e48f8c6ea56a92fa71771
iminyukhin <minyukhin.ilya@gmail.com>
2019-08-23 15:42:28 +0600

    #25 add tests for 'show devices with last user'

--- src/test/java/net/thumbtack/testdevices/web/services/DevicesServiceTest.java
+++ src/test/java/net/thumbtack/testdevices/web/services/DevicesServiceTest.java
@@ -2,11 +2,16 @@ package net.thumbtack.testdevices.web.services;
 
 import net.thumbtack.testdevices.core.models.Device;
 import net.thumbtack.testdevices.core.models.DeviceType;
+import net.thumbtack.testdevices.core.models.DeviceWithLastUser;
 import net.thumbtack.testdevices.core.repositories.DeviceDao;
 import net.thumbtack.testdevices.dto.request.DeviceRequest;
+import net.thumbtack.testdevices.dto.response.DeviceWithLastUserResponse;
 import net.thumbtack.testdevices.web.converters.DeviceDtoToModelConverter;
+import net.thumbtack.testdevices.web.converters.DeviceWithLastUserDtoToModelConverter;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -16,12 +21,18 @@ public class DevicesServiceTest {
     private DeviceRequest request;
     private Device device;
     private DeviceDtoToModelConverter deviceDtoToModelConverter;
+    private DeviceWithLastUserDtoToModelConverter deviceWithLastUserDtoToModelConverter;
+    private List<DeviceWithLastUser> deviceWithLastUserList;
+    private List<DeviceWithLastUserResponse> deviceWithLastUserResponseList;
 
     @Before
     public void setup() {
+        deviceWithLastUserList = mock(List.class);
+        deviceWithLastUserResponseList = mock(List.class);
         deviceDao = mock(DeviceDao.class);
         deviceDtoToModelConverter = mock(DeviceDtoToModelConverter.class);
-        devicesServiceImpl = spy(new DevicesServiceImpl(deviceDao, deviceDtoToModelConverter));
+        deviceWithLastUserDtoToModelConverter = mock(DeviceWithLastUserDtoToModelConverter.class);
+        devicesServiceImpl = spy(new DevicesServiceImpl(deviceDao, deviceDtoToModelConverter, deviceWithLastUserDtoToModelConverter));
         request = new DeviceRequest(
                 DeviceType.PHONE.getDeviceType(),
                 "Apple",
@@ -54,5 +65,20 @@ public class DevicesServiceTest {
     public void testDeleteDevice() {
         devicesServiceImpl.deleteDevice(1L);
         verify(deviceDao, times(1)).deleteById(eq(1L));
+    }
+
+    @Test
+    public void testGetDevicesWithLastUserWhoTakenDevice() {
+        when(deviceDao.getDevicesWithLastUserWhoTakenDevice("%search%")).thenReturn(deviceWithLastUserList);
+        when(
+                deviceWithLastUserDtoToModelConverter.getDeviceWithLastUserResponseListFromDeviceWithLastUserList(deviceWithLastUserList)
+        ).thenReturn(deviceWithLastUserResponseList);
+
+        devicesServiceImpl.getDevicesWithLastUserWhoTakenDevice("search");
+
+        verify(deviceDao, times(1)).getDevicesWithLastUserWhoTakenDevice(eq("%search%"));
+        verify(
+                deviceWithLastUserDtoToModelConverter, times(1)
+        ).getDeviceWithLastUserResponseListFromDeviceWithLastUserList(deviceWithLastUserList);
     }
 }
